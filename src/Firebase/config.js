839c1f8059fd136d04ab72b85aceddaa7c8c384b
AmiYamanouchi <joe.b11s35@gmail.com>
2021-04-07 13:29:35 -0700

    first commit

---
+++ src/Firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBXUPqd2sCgDQSJqRioVDGBWTnirPyBBng",
+    authDomain: "my-chatbot-d80af.firebaseapp.com",
+    projectId: "my-chatbot-d80af",
+    storageBucket: "my-chatbot-d80af.appspot.com",
+    messagingSenderId: "784898463722",
+    appId: "1:784898463722:web:489e728901cd4826df8493",
+    measurementId: "G-XPT5B4DXG1"
+  };
+
+  export default firebaseConfig
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/styles/index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <>
+    <App />
+  </>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
